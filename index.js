97e509db8a17c9dcc20a983ebf318106338e3b50
Nick Correa <nickcorreagarcia@gmail.com>
2020-07-30 22:29:16 -0500

    index.js finalizado

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('start')
+function computerPlay() {
+  // will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+
+  var res = 0;
+
+  res = getRandomInt(1, 4);
+  if (res === 1) return "rock";
+  else if (res === 2) return "paper";
+  else {
+    console.log("saio scissors");
+    return "scissors";
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(computerPlay());
